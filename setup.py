34f7f61e1c565a697e152b200d8f456bc4d9918f
Changbeom Choi <cbchoi@hanbat.ac.kr>
2023-04-09 18:08:47 +0900

    version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pyevsim",
-    version="1.1.3",
+    version="1.1.5",
     license='MIT',
     author="Changbeom Choi",
     author_email="me@cbchoi.info",
